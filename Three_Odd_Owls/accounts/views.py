ae442412f16d6aeec7d8ec07eaabe9f90d986d70
Keenan <keenandup87@gmail.com>
2023-03-20 14:46:12 +0200

    update docstrings

--- Three_Odd_Owls/accounts/views.py
+++ Three_Odd_Owls/accounts/views.py
@@ -1,9 +1,22 @@
+"""
+View functions for user registration and login.
+"""
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as auth_login
 
 def register(request):
+    """
+    View function for user registration.
+
+    This view function handles GET and POST requests to the 'register/'
+    URL. If a GET request is received, it returns a rendered template with a
+    UserCreationForm. If a POST request is received, it attempts to validate
+    the form data and create a new user. If the form data is valid, the user is
+    created and the user is redirected to the 'login/' URL. If the form data is
+    invalid, the form is re-rendered with error messages.
+    """
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
@@ -14,6 +27,17 @@ def register(request):
     return render(request, 'registration/register.html', {'form': form})
 
 def login(request):
+    """
+    View function for user login.
+
+    This view function handles GET and POST requests to the 'login/'
+    URL. If a GET request is received, it returns a rendered template with an
+    AuthenticationForm. If a POST request is received, it attempts to validate
+    the form data and log the user in. If the form data is valid, the user is
+    logged in and redirected to the 'home' URL. If the form data is invalid,
+    the form is re-rendered with error messages.
+
+    """
     if request.method == 'POST':
         form = AuthenticationForm(request, request.POST)
         if form.is_valid():
