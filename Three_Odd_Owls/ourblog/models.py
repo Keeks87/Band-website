8e2b14eb805ccda4987f4cf1da285fdf453d6b26
Keenan <keenandup87@gmail.com>
2023-03-20 14:59:48 +0200

    update docstrings

--- Three_Odd_Owls/ourblog/models.py
+++ Three_Odd_Owls/ourblog/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 class Post(models.Model):
+    """
+    A model representing a blog post.
+
+    Attributes:
+        title (str): The title of the post.
+        body (str): The body of the post.
+        date (datetime): The date and time when the post was created.
+    """
 
     title = models.CharField(max_length=140)
     body = models.TextField()
@@ -11,6 +19,15 @@ class Post(models.Model):
         return self.title
 
 class Comment(models.Model):
+    """
+    A model representing a comment on a blog post.
+
+    Attributes:
+        post (Post): The post that the comment is associated with.
+        author (User): The user who wrote the comment.
+        body (str): The body of the comment.
+        created_on (datetime): The date and time when the comment was created.
+    """
     post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
